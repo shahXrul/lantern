683635b9b767de8bfe661dcdc7c85212fb5cc64f
shahrul <shahrul.hussain.shahul.hamid@intel.com>
2022-12-01 23:41:07 +0800

    update c#

--- lanternApi/Program.cs
+++ lanternApi/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+camelCasing();
+
+int camelCasing(){
+    var a = 1;
+    return a;
+}
+
+int PascalCasing(){
+    var b = 2;
+    return b;
+}
