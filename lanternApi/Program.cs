f54fc6129661f39f8c58c1a74dc7d8a1d3aa0771
shahXrul <99777822+shahXrul@users.noreply.github.com>
2022-12-02 01:00:30 +0800

    Develop (#14)
    * pr0
    
    * fixed
    
    * [MegaLinter] Apply linters fixes
    
    * added c#
    
    * put
    
    * update worklflow
    
    * udapte workflow again
    
    * again
    
    * again2
    
    * [MegaLinter] Apply linters fixes
    
    * again
    
    * update c#
    
    * [MegaLinter] Apply linters fixes
    
    * update severity
    
    * added config
    
    * [MegaLinter] Apply linters fixes
    
    Co-authored-by: shahXrul <shahXrul@users.noreply.github.com>

---
+++ lanternApi/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+camelCasing();
+PascalCasing();
+
+int camelCasing()
+{
+    const int Pokemonjhoto = 1;
+    var pikachu = 2;
+    var a = 1;
+    if (a == 1)
+    {
+        pikachu = 10;
+    }
+    return a;
+}
+
+int PascalCasing()
+{
+    var b = 2;
+    return b;
+}
+
+int pichuonetwo()
+{
+    return 1;
+}
